58f4d557c876733c0bf424ba2aad92634775ead2
Mike Bell <mdb036@gmail.com>
2025-08-31 10:26:24 +0100

    Distinct project name

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_tt_tinyQV #(parameter CLOCK_MHZ=64) (
+module tt_um_tt_tinyQVb #(parameter CLOCK_MHZ=64) (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path - only some bits used
--- test/tb.v
+++ test/tb.v
@@ -50,7 +50,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_tt_tinyQV user_project (
+  tt_um_tt_tinyQVb user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
