9eb3f4932eaadc9d403558fe5eaa15dff6286a68
Ciro Cattuto <ciro.cattuto@gmail.com>
2025-09-04 09:48:49 +0800

    VGA scope peripheral

--- src/peripherals.v
+++ src/peripherals.v
@@ -279,7 +279,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .user_interrupt(user_interrupts[6])
     );
 
-    tqvp_full_empty i_user_peri07 (
+    tqvp_cattuto_vgascope i_user_peri07 (
         .clk(clk),
         .rst_n(rst_n),
 
