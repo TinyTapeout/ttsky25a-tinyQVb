f69086037d6ed71bc3783aac50b44a3a26bef58f
adityavs674 <harshadmehta1919@gmail.com>
2025-09-05 18:32:57 +0530

    Update peripherals.v to add my peripheral in slot 10

--- src/peripherals.v
+++ src/peripherals.v
@@ -337,7 +337,7 @@ module tinyQV_peripherals #(parameter CLOCK_MHZ=64) (
         .user_interrupt(user_interrupts[9])
     );
 
-    tqvp_full_empty i_user_peri10 (
+    tqvp_noclueALU i_user_peri10 (
         .clk(clk),
         .rst_n(rst_n),
 
