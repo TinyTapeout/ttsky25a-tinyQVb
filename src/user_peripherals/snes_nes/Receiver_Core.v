46f88ec343df0b53afc8005ade9c7aa2644c333c
James <Jashiekotey@gmail.com>
2025-09-06 07:06:36 +0100

    Add gamepad pmod simgle back in

--- src/user_peripherals/snes_nes/Receiver_Core.v
+++ src/user_peripherals/snes_nes/Receiver_Core.v
@@ -428,3 +428,56 @@ module NES_Reciever (
 
 endmodule
 
+module gamepad_pmod_single (
+    input wire rst_n,
+    input wire clk,
+    input wire pmod_data,
+    input wire pmod_clk,
+    input wire pmod_latch,
+
+    output wire b,
+    output wire y,
+    output wire select,
+    output wire start,
+    output wire up,
+    output wire down,
+    output wire left,
+    output wire right,
+    output wire a,
+    output wire x,
+    output wire l,
+    output wire r,
+    output wire is_present
+);
+
+  wire [11:0] gamepad_pmod_data;
+
+  gamepad_pmod_driver #(
+      .BIT_WIDTH(12)
+  ) driver (
+      .rst_n(rst_n),
+      .clk(clk),
+      .pmod_data(pmod_data),
+      .pmod_clk(pmod_clk),
+      .pmod_latch(pmod_latch),
+      .data_reg(gamepad_pmod_data)
+  );
+
+  gamepad_pmod_decoder decoder (
+      .data_reg(gamepad_pmod_data),
+      .b(b),
+      .y(y),
+      .select(select),
+      .start(start),
+      .up(up),
+      .down(down),
+      .left(left),
+      .right(right),
+      .a(a),
+      .x(x),
+      .l(l),
+      .r(r),
+      .is_present(is_present)
+  );
+
+endmodule
